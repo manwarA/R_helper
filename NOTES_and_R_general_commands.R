dfc21ca432d9c73905e1ec3d9804c5a51fb51a52
Ayaz Anwar <73974893+manwarA@users.noreply.github.com>
2025-12-13 18:47:41 +0100

    Update NOTES_and_R_general_commands.R

--- NOTES_and_R_general_commands.R
+++ NOTES_and_R_general_commands.R
@@ -831,6 +831,25 @@ confusionMatrix(data=pred, caret.test$status2)# not working
 f1 <- pROC::roc(status2 ~ model.cv.f5, data = caret.test) 
 plot(f1)
 
+
+#======================================
+# linear model diagnostic
+#======================================
+
+
+
+						In addition to examining the diagnostic plots, it may be interesting and useful to examine, for each data point in turn, how removal of that point affects the regression coefficients, prediction and so on. To get these values, R has corresponding function to use: diffs(), dfbetas(), covratio(), hatvalues() and cooks.distance(). For example, we assess how many standard errors the predicted value changes when the ith observation is removed via the following command. (Note that here it doesn't show the result.)
+> diffs(lm3)
+Also, we can identify the leverage point via
+> # list the observation with large hat value
+> lm3.hat <- hatvalues(lm3)
+> id.lm3.hat <- which(lm3.hat > (2*(4+1)/nrow(fatdata))) ##  hatvalue > #2*(k+1)/n
+> lm3.hat[id.lm3.hat]
+         5             9              12              28         39         79        106
+0.04724447 0.04100957 0.05727609 0.06020518 0.17631101 0.04596512 0.06125064
+       207             216            235
+0.04501627 0.05087598 0.05863139
+It indicates potential influential observations for 10 data points. This tells us that we need to pay attention to observations 5, 9, 12, 28, 39, 79, 106, 207, 216 and 235. If we also see these points standing out in other diagnostics, then more investigation might be warned.
 #==================================
 # Model evaluation methods
 #==================================
@@ -1523,6 +1542,7 @@ snippet ss
 	#=========================================
 	#
 	#=========================================
+
 
 
 
